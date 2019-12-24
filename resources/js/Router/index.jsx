ed699b060b44fcc59db1540b3ba47208d9836b2f
Vlad <vlad2009_@ukr.net>
2019-12-24 20:09:05 +0200

    adaptability page Cart and total order amount

--- resources/js/Router/index.jsx
+++ resources/js/Router/index.jsx
@@ -9,7 +9,7 @@ class Router extends Component {
         return (
             <BrowserRouter>
                 < Switch>
-                    <Route path='/'
+                    <Route path='/cart'
                            exact component={Cart}
                     />
                     <Route path='/shipping'
--- resources/js/components/Cart.jsx
+++ resources/js/components/Cart.jsx
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getProducts } from '../actions/ProductActions';
 import map from 'lodash/map';
 import '../styles/CartStyle.css';
 import Image from "../images/image.png";
-import { constants } from '../constants';
 
 class Cart extends Component {
 
     state = {
         numbers: [],
         prices: [],
-        sum: null,
-        s: null,
     }
 
     componentDidMount() {
@@ -32,6 +30,7 @@ class Cart extends Component {
     handleChange = (index) => {
         let numbers = [...this.state.numbers]
         numbers[index] = event.target.value
+
         if (numbers[index] > 0 && numbers[index] < 50) {
             this.setState({
                 numbers
@@ -42,17 +41,21 @@ class Cart extends Component {
     handleMove = (index, step) => {
         let numbers = [...this.state.numbers]
         numbers[index] = numbers[index] + step
+        let prices = [...this.state.prices]
+        prices[index] = this.props.products[index].price * numbers[index]
         if (numbers[index] > 0 && numbers[index] <= 50) {
             this.setState({
-                numbers
+                numbers,
+                prices,
             })
         }
     }
 
     render() {
         const { products } = this.props;
-        console.log('products', products);
-        console.log('prices', this.state.prices)
+        let sum = 0;
+        this.state.prices.forEach(price => { sum += price })
+
         return (
             <div className='container'>
                 {products &&
@@ -67,14 +70,11 @@ class Cart extends Component {
                             </div>
                             <div className='number'>
                                 <button className='btnMinus' onClick={() => this.handleMove(index, -1)}>-</button>
-                                <input
-                                    type="number"
+                                <p
                                     name='number'
-                                    min='1'
-                                    max='50'
-                                    value={this.state.numbers[index] || 1}
+                                    // value={this.state.numbers[index] || 1}
                                     onChange={() => this.handleChange(index)}
-                                />
+                                >{this.state.numbers[index] || 1}</p>
                                 <button className='btnPlus' onClick={() => this.handleMove(index, +1)}>+</button>
                             </div>
                             <div className='price'>
@@ -84,9 +84,18 @@ class Cart extends Component {
                     ))}
                 <div className='total'>
                     <div className="sum">
-                        <p> €</p>
+                        <p>
+                            {sum} €
+                        </p>
                     </div>
-                    <button className="btnBuy">BUY</button>
+                    <Link to="/shipping">
+                        <button
+                            className="btnBuy"
+                            sum={sum}
+                        >
+                            BUY
+                        </button>
+                    </Link>
                 </div>
             </div>
         )
